7147d3d7a0600be4ab3944892a85ef281bd1ca75
Restyled.io <commits@restyled.io>
2019-05-20 11:40:43 +0000

    Restyled by prettier

--- fixtures/assets/vue/router/index.js
+++ fixtures/assets/vue/router/index.js
@@ -1,32 +1,32 @@
-import Router from 'vue-router';
+import Router from "vue-router";
 
-import Dashboard from '../components/Dashboard';
-import Register from '../components/Register';
-import ResetPasswordConfirm from '../components/ResetPasswordConfirm';
-import NotFound from '../components/NotFound';
+import Dashboard from "../components/Dashboard";
+import Register from "../components/Register";
+import ResetPasswordConfirm from "../components/ResetPasswordConfirm";
+import NotFound from "../components/NotFound";
 
 export default new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'homepage',
-            component: Dashboard
-        },
-        {
-            path: '/register',
-            name: 'registerUser',
-            component: Register
-        },
-        {
-            path: '/reset-password/confirm/:token',       // * => wildcard. Matches all other routes
-            name: 'resetPasswordConfirm',
-            component: ResetPasswordConfirm
-        },
-        {
-            path: '*',       // * => wildcard. Matches all other routes
-            name: 'notfound',
-            component: NotFound
-        }
-    ]
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "homepage",
+      component: Dashboard
+    },
+    {
+      path: "/register",
+      name: "registerUser",
+      component: Register
+    },
+    {
+      path: "/reset-password/confirm/:token", // * => wildcard. Matches all other routes
+      name: "resetPasswordConfirm",
+      component: ResetPasswordConfirm
+    },
+    {
+      path: "*", // * => wildcard. Matches all other routes
+      name: "notfound",
+      component: NotFound
+    }
+  ]
+});
